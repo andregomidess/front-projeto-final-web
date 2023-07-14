68164a54097ef2ff2250889d5b15c1b459fa0e3e
andre <andreluccag@gmail.com>
2023-07-14 01:09:28 -0300

    feat: add fav

--- src/app/components/side-bar/navegacao/navegacao.component.ts
+++ src/app/components/side-bar/navegacao/navegacao.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { User } from 'src/app/models/User.model';
+import { ConteudoPaginaService } from 'src/app/pages/conteudo-pagina/service/conteudo-pagina.service';
 
 @Component({
   selector: 'app-navegacao',
@@ -8,9 +10,17 @@ import { Component } from '@angular/core';
 export class NavegacaoComponent {
 
   selectedItem: string = '';
+  @Input() userData!: User;
+
+
+  constructor(public conteudoPaginaService: ConteudoPaginaService) {}
 
   changeBackgroundColor(item: string) {
     this.selectedItem = item;
   }
+
+  
+
+
 
 }
